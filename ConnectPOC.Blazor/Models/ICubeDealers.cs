e6f651f99cf136863f0d1972d699492b59b32246
amarpreet_singh <Amarpreet.Singh@tvsd.ai>
2025-04-09 10:52:14 +0530

    changes to show dealer details in UI

---
+++ ConnectPOC.Blazor/Models/ICubeDealers.cs
@@ -0,0 +1,9 @@
+﻿namespace ConnectPOC.Blazor.Models
+{
+    public class ICubeDealers
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string DelarCode { get; set; }
+    }
+}
